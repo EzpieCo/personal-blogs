4bd7949497373ee538a36bef79a63159a4f5f686
Ishaan Singh <ezpie.co@gmail.com>
2024-08-10 22:28:54 +0530

    update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -19,7 +19,8 @@ const Bio = () => {
             summary
           }
           social {
-            twitter
+            github
+            dev
           }
         }
       }
@@ -36,7 +37,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="../images/profile.png"
         width={50}
         height={50}
         quality={95}
@@ -45,9 +46,13 @@ const Bio = () => {
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          {" "}
+          Follow on {" "}
+          <a href={`https://github.com/${social.github || ``}`}>
+            Github
+          </a> and {" "}
+          <a href={`https://dev.to/${social.dev}`}>
+            Dev
           </a>
         </p>
       )}
